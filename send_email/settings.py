c2cd96117659186bb84c5e62bddf7ce07c19bad2
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-08-16 15:05:18 +0300

    Update settings.py

--- send_email/settings.py
+++ send_email/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 #smtp
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'reqww00@gmail.com'
-EMAIL_HOST_PASSWORD = 'CFHFYXF228hec;'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
